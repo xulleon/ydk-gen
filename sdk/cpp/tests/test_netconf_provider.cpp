52a8c44dcd23cc940eaf62f9256cf881aa847b91
ygorelik <ygorelik@cisco.com>
2019-03-30 11:02:04 -0700

    Fixing Travis test

--- sdk/cpp/tests/test_netconf_provider.cpp
+++ sdk/cpp/tests/test_netconf_provider.cpp
@@ -39,7 +39,7 @@ TEST_CASE("CreateNoRepoP")
 }
 
 // NOTE: The ./ ensures this test has to be explicitly run by name
-TEST_CASE("CreateNoRepoPTCP")
+TEST_CASE("./CreateNoRepoPTCP")
 {
     NetconfServiceProvider provider{ "127.0.0.1", "admin", "admin", 12307, "tcp"};
     CHECK_NOTHROW(provider.get_encoding());
